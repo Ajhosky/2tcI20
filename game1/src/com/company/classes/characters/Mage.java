6676cffe23c04d46306d03ce2030ed7c66336094
Ajhosky <dsobolewski@tm1.edu.pl>
2022-03-30 09:15:53 +0200

    attack right fixed

--- game1/src/com/company/classes/characters/Mage.java
+++ game1/src/com/company/classes/characters/Mage.java
@@ -25,15 +25,19 @@ public class Mage  extends CharacterClass {
     }
     public void left() {
         int newPositionX = this.getX() > 40 ?  this.getX() - 40 : 320;
+        tryChangePosition(newPositionX, this.getY());
     }
     public void right() {
         int newPositionX = this.getX() < 320 ?  this.getX() + 40 : 0;
+        tryChangePosition(newPositionX, this.getY());
 
     }
     public void up() {
-        int newPositionY = this.getY() > 80 ?  this.getY() - 80 : 320;
+        int newPositionY = this.getY() >= 80 ?  this.getY() - 80 : 320;
+        tryChangePosition(this.getX(), newPositionY);
     }
     public void down() {
         int newPositionY = this.getY() < 320 ?  this.getY() + 80 : 0;
+        tryChangePosition(this.getX(), newPositionY);
     }
 }
